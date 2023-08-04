55363c60cde2ccf1dc5bf8c1061e045fcd8d9c43
Cinzia <1004700282@qq.com>
2023-08-04 11:43:23 +0800

    调试

---
+++ src/main/java/com/ssh/V1SshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1SshBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V1SshBackendApplication.class, args);
+    }
+
+}
